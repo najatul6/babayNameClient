d4056cd0b2e6314f5a5c33c7bf7ce28cda46fc94
Md Najatul Islam <mdnajatulislam.develop@gmail.com>
2025-03-18 00:14:08 +0600

    Refactor JourneyCards component: enhance props for dynamic content rendering

--- src/components/RootView/common/JourneyCards.jsx
+++ src/components/RootView/common/JourneyCards.jsx
@@ -1,12 +1,13 @@
-import image from "../../../assets/Pregnant in love.jpeg"
-const JourneyCards = () => {
+import PropTypes from "prop-types"
+
+const JourneyCards = ({ banner, title, description }) => {
     return (
         <div className="border rounded-lg shadow-md p-4 max-w-sm bg-white animate__animated animate__fadeIn">
             <div className="relative border">
 
                 <div className="overflow-hidden rounded-lg">
                     <img
-                        src={image}
+                        src={banner}
                         alt="Pregnancy Test"
                         className="w-full h-48 object-cover "
                     />
@@ -14,18 +15,23 @@ const JourneyCards = () => {
                 <div className="absolute -bottom-[1.50rem] w-full flex justify-center items-center">
 
                     <h2 className="inline-block bg-blue-900 text-white text-lg font-bold py-2 px-4  rounded-md animate__animated animate__bounceIn">
-                        Getting Pregnant
+                        {title}
                     </h2>
                 </div>
             </div>
-            <div className="text-center mt-4">
-
+            <div className="text-center mt-10">
                 <p className="text-gray-700 mt-2 px-4">
-                    From fertility signs to look for to ways of upping your odds of conception, get actionable advice that’ll set you on a path to parenthood.
+                    {description}
                 </p>
             </div>
         </div>
     )
+}
+
+JourneyCards.propTypes = {
+    banner: PropTypes.string,
+    title: PropTypes.string,
+    description: PropTypes.string
 }
 
 export default JourneyCards
