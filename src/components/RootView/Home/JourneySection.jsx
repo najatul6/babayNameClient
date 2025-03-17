3476f96b43c0d892cfa50a181e110a8348905519
Md Najatul Islam <mdnajatulislam.develop@gmail.com>
2025-03-18 00:19:31 +0600

    Update journey titles and subtitles for improved clarity; fetch journey data in JourneySection component

--- src/components/RootView/Home/JourneySection.jsx
+++ src/components/RootView/Home/JourneySection.jsx
@@ -1,10 +1,28 @@
 import SectionHeader from "@/components/common/SectionHeader"
 import JourneyCards from "../common/JourneyCards"
+import { useEffect, useState } from "react"
 
 const JourneySection = () => {
+  const [journeyCards, setJourneyCards] = useState([])
+
+  useEffect(() => {
+    fetch("/journey.json")
+      .then((response) => response.json())
+      .then((data) => setJourneyCards(data))
+  },[])
   return (
     <div>
         <SectionHeader title="Your Journey Starts Here"/>
+        <div className="grid grid-cols-1 md:grid-cols-3 gap-4">
+          {journeyCards.map((card) => (
+            <JourneyCards
+              key={card.id}
+              banner={card.banner}
+              title={card.title}
+              description={card.description}
+            />
+          ))}
+        </div>
         <JourneyCards/>
     </div>
   )
