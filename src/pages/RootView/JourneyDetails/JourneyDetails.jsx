2463f33ecb8cc482214a15fae08ebe55886bbd6a
Md Najatul Islam <mdnajatulislam.develop@gmail.com>
2025-03-18 00:53:34 +0600

    Enhance JourneyDetails component styling for improved readability and responsiveness

--- src/pages/RootView/JourneyDetails/JourneyDetails.jsx
+++ src/pages/RootView/JourneyDetails/JourneyDetails.jsx
@@ -18,18 +18,17 @@ const JourneyDetails = () => {
     if (!cardDetails) return <p className="text-center text-lg">Loading...</p>;
 
     return (
-        <div className="p-4 w-full mx-auto flex flex-col items-center">
+        <div className="p-4 w-full mx-auto flex flex-col items-center border">
             <div className="w-full relative">
-
-            <img src={cardDetails.logo} alt={cardDetails.title} className="w-full max-h-96 object-cover rounded-lg" />
-           <div className="absolute top-0 left-0 right-0 bottom-0 bg-gray-900 bg-opacity-50 flex flex-col justify-center items-center">
-           <h1 className="text-3xl font-bold mt-4 text-center text-white">{cardDetails.title}</h1>
-            <h2 className="text-lg text-gray-200 text-center">{cardDetails.subTitle}</h2>
-            <p className="mt-2 text-gray-300 text-center px-4 md:px-8">{cardDetails.description}</p>
-           </div>
+                <img src={cardDetails.logo} alt={cardDetails.title} className="w-full max-h-96 object-cover rounded-lg" />
+                <div className="absolute top-0 left-0 right-0 bottom-0 bg-gray-900 bg-opacity-50 flex flex-col justify-center items-center">
+                    <h1 className="text-lg md:text-3xl font-bold mt-4 text-center text-white">{cardDetails.title}</h1>
+                    <h2 className="text-md md:text-lg text-gray-200 text-center">{cardDetails.subTitle}</h2>
+                    <p className="mt-2 text-gray-300 text-center px-4 md:px-8 text-sm md:text-base">{cardDetails.description}</p>
+                </div>
             </div>
 
-            <div className="mt-6 w-full grid  grid-cols-1 md:grid-cols-2 gap-4">
+            <div className="mt-6 w-full grid grid-cols-1 md:grid-cols-2 gap-4">
                 <div className="mt-6 w-full px-4 md:px-8">
                     <h3 className="text-xl font-semibold">Introduction</h3>
                     <p>{cardDetails.content.introduction}</p>
